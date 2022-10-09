26253a5e6672f19ed1819e031bcc018f6a0b1a41
BANVIENCORP\hoang.ngo <hoang.ngo@banvien.com.vn>
2022-10-09 15:00:59 +0700

    revise docker-compose.yml for config-server

--- src/main/resources/db.migration/V20220911121141__create_table_location.sql
+++ src/main/resources/db.migration/V20220911121141__create_table_location.sql
@@ -1,19 +1,5 @@
 set search_path to mhmarket;
 
-create function gen_random_uuid() returns uuid
-    strict
-    cost 1
-    language internal
-as
-$$
-begin
--- missing source code
-end;
-$$;
-
-comment on function gen_random_uuid() is 'generate random UUID';
-
-
 create table location
 (
     location_id uuid primary key default gen_random_uuid(),
